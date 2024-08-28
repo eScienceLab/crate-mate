4d8fdd0b61c6ca3ec73ab2d63b10ab5fdb074691
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2024-08-28 07:33:06 +0100

    First functioning test

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,9 +1,24 @@
+import logging
 import pytest
-from src import main 
 
-def test_correct_root():
+from src import main
+
+# ------------------------------------------------------------------------------
+# Fixtures
+
+@pytest.fixture
+def logger():
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+# ------------------------------------------------------------------------------
+# Tests
+
+def test_correct_root(logger):
     # call main on example.json
-    main.main()
+    main.main(logger=logger, input_path="/workspace/example.json")
 
     # assert that the output is correct
     assert True == True
